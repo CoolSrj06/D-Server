eeb7d82abe304ca229a39bab4272f352f36f036d
CoolSrj06 <srijanmaurya6602@gmail.com>
2025-01-17 13:24:59 +0530

    Add initial project setup with Express, MongoDB connection, and configuration files

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+// app.use() is a function in Express.js that allows you to add middleware to your application
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public")) 
+app.use(cookieParser())
+
+
+//routes import
+//import userRoute from './routes/user.routes.js';
+//import { errorHandler } from './middleware/errorHandler.js';
+
+
+//routes declaration
+//app.use("/api/v1/", userRoute)
+
+// Error handling middleware (must be after all routes)
+//app.use(errorHandler);
+
+export { app }
