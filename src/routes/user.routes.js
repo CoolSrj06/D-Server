aac55fe454d95bf4badaec3f583b506477dba4c8
CoolSrj06 <srijanmaurya6602@gmail.com>
2025-02-22 12:47:18 +0530

    Add routes for downloading survey data and fetching report data

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,16 +11,30 @@ const router = Router();
 app.use(express.static('../'));
 app.use('/', router)
 
+// push 
 router.route('/postSurvey').post(postSurvey);
 router.route('/postSurveyForm').post(postSurveyForm);
+
+// upload excel file on server
 router.route('/upload').post(upload.single('file'),uploadExcelSurveyData);
+
+// User Signup
 router.post('/signUp',handleUserSignUp);
+
+// Admin and Sales Login
 router.post('/adminLogin', handleAdminLogin);
 router.post('/salesLogin', handleSalesLogin);
+
+// push CSV data from server to mongodb storage
 router.route('/pushCSVData').post(pushCSVData);
 
+// download survey data from mongodb storage
 router.route('/downloadSurveyData').get(downloadSurveyData);
+
+// paginated CSV data from mongodb storage to frontend
 router.route('/paginatedCSVData').get(paginatedCSVData);
+
+// fetch report data from mongodb storage and send to frontend
 router.route('/handleReport').get(handleReport);
 
 
