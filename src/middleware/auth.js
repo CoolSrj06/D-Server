7aac033963916febf5dc55be7b30ceefb143e66d
CoolSrj06 <srijanmaurya6602@gmail.com>
2025-02-27 15:14:21 +0530

    Add JWT authentication middleware for setting featured reports

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+import { asyncHandler} from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import jwt from "jsonwebtoken";
+import { User } from "../model/admin.model.js"
+
+export const verifyJWT = asyncHandler(async(req, _ , next) => { // inplace of res we can use ' _ '
+    try {
+        const token = req.cookies?.accessToken ||  req.header("Authorization")?.replace("Bearer ", "")
+        
+        if (!token) {
+            throw new ApiError(401, "Unauthorized", ["Token is missing"]);
+        }        
+        
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodedToken?._id).select(" -password -refreshToken")
+        
+        if(!user) {
+            throw new ApiError(401, "Invalid Access Token")
+        }
+    
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid Access Token")  
+    }
+})
--- src/routes/featured.routes.js
+++ src/routes/featured.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import express from "express";
 
 import { setFeatured, featuredReports, fullDetailOfFeaturedReports } from "../controller/featured.controller.js";
+import { verifyJWT } from "../middleware/auth.js"
 
 const app = express();
 const router = Router();
@@ -9,7 +10,7 @@ const router = Router();
 app.use(express.static('../'));
 app.use('/featured', router)
 
-router.route('/setFeatured').post(setFeatured);
+router.route('/setFeatured').post(verifyJWT, setFeatured);
 router.route('/featuredReports').get(featuredReports);
 router.route('/fullDetailOfFeaturedReports').get(fullDetailOfFeaturedReports);
 
